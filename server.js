538a25fdf248376ae212953dbd833f85b3432d77
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-06-17 18:34:54 -0400

    still stuck

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
+const writeFileP = (filePath, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, JSON.stringify(data), (err) => {
+            if (err) {
+                return reject(err)
+            }
+            else {
+                resolve();
+            }
+        })
+    })
+}
+
+const readFileP = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (err,data) => {
+            if (err) {
+                return reject(err)
+            }
+            else {
+                let results;
+                try {
+                    results = JSON.parse(data.toString())
+                }
+                catch(ex){
+                    return reject(ex);
+                }
+                resolve(results);
+            }
+        })
+    })
+}
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
-app.use(express.static(path.join(__dirname, './dist/main.js')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index2.html'))
+})
 
-
+app.get('/api/products', (req, res) => {
+    res.readFileP('./data.json')
+        .then(data => {
+            
+        })
+})
 
 app.listen(PORT);
